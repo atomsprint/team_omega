bb5ba5841fb12d6b5dfaaf73216d0bb762f0f89b
Rikuto <93rainbow@gmail.com>
2026-02-11 19:53:08 +0900

    クイズ追加２

--- Omega_castle/PrefectureData.swift
+++ Omega_castle/PrefectureData.swift
@@ -14,10 +14,9 @@ class PrefectureData {
         Prefecture(rank: 3, name: "二条城", area: 2046329.00),
         Prefecture(rank: 4, name: "姫路城", area: 1532111.00),
         Prefecture(rank: 5, name: "熊本城", area: 1419664.00),
-        Prefecture(rank: 1, name: "松本城", area: 982700.00),
-        Prefecture(rank: 2, name: "彦根城", area: 651149.00),
-        Prefecture(rank: 3, name: "小田原城", area: 589485.00),
-        Prefecture(rank: 4, name: "高知城", area: 274652.00)
-        
+        Prefecture(rank: 6, name: "松本城", area: 982700.00),
+        Prefecture(rank: 7, name: "彦根城", area: 651149.00),
+        Prefecture(rank: 8, name: "小田原城", area: 589485.00),
+        Prefecture(rank: 9, name: "高知城", area: 274652.00)
     ]
 }
