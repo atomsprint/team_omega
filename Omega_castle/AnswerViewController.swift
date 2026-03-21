01af2f6d33a89eb912dae711d7372fa488a8fada
Haruno <haruno8128@gmail.com>
2026-03-21 13:50:30 +0900

    解説画面を追加した。

---
+++ Omega_castle/AnswerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AnswerViewController.swift
+//  Omega_castle
+//
+//  Created by Takahashi Haruno on 2026/03/21.
+//
+
+import UIKit
+
+class AnswerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
