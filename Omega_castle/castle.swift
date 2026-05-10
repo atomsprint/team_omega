ceba2b6a05a2e969aaa4dd25742699ecd54264fa
Rikuto <93rainbow@gmail.com>
2026-05-11 06:55:35 +0900

    castlesにお城情報を入れた

--- Omega_castle/castle.swift
+++ Omega_castle/castle.swift
@@ -12,17 +12,22 @@ class Castle {
     var BuiltPeople: String
     var alias: String
     var English: String
+    var URL: String
+    var place: String
     var category1: String
     var category2: String
     var category3: String
     var category4: String
     var Explanation: String
-
+    var region: String
     init(row: [String]) {
         self.CastleName = row.count > 0 ? row[0] : "不明"
         self.BuiltPeople = row.count > 1 ? row[1] : "不明"
         self.English = row.count > 4 ? row[4] : "不明"
         self.alias = row.count > 5 ? row[5] : "不明"
+        self.URL = row.count > 6 ? row[6] : "不明"
+        self.place = row.count > 7 ? row[7] : "不明"
+        self.region = row.count > 3 ? row[3] : "不明"
         
         self.category1 = row.count > 10 ? row[10].trimmingCharacters(in: .whitespacesAndNewlines) : ""
         self.category2 = row.count > 11 ? row[11].trimmingCharacters(in: .whitespacesAndNewlines) : ""
