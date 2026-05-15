7f4c66959ba72eb9d2ab33ee673b961ea5eb9836
Haruno <haruno8128@gmail.com>
2026-05-15 18:02:33 +0900

    お城の画像を入れた。

--- Omega_castle/ExplanationViewController.swift
+++ Omega_castle/ExplanationViewController.swift
@@ -8,7 +8,66 @@
 import UIKit
 
 class ExplanationViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var Castle_Image: UIImageView!
+    @IBOutlet weak var Castle_Name: UILabel!
+    @IBOutlet weak var Castle_Explanation: UILabel!
+    
+    @IBAction func Goryokaku(_ sender: Any) {
+    }
+    @IBAction func Hirosaki(_ sender: Any) {
+    }
+    @IBAction func Sendai(_ sender: Any) {
+    }
+    @IBAction func Yamagata(_ sender: Any) {
+    }
+    @IBAction func Aizuwakamatu(_ sender: Any) {
+    }
+    @IBAction func Oshi(_ sender: Any) {
+    }
+    @IBAction func Odawara(_ sender: Any) {
+    }
+    @IBAction func Ueda(_ sender: Any) {
+    }
+    @IBAction func Matumoto(_ sender: Any) {
+    }
+    @IBAction func Kanazawa(_ sender: Any) {
+    }
+    @IBAction func Maruoka(_ sender: Any) {
+    }
+    @IBAction func Inuyama(_ sender: Any) {
+    }
+    @IBAction func Nagoya(_ sender: Any) {
+    }
+    @IBAction func Nagashino(_ sender: Any) {
+    }
+    @IBAction func Hikone(_ sender: Any) {
+    }
+    @IBAction func Azuchi(_ sender: Any) {
+    }
+    @IBAction func Osaka(_ sender: Any) {
+    }
+    @IBAction func Takeda(_ sender: Any) {
+    }
+    @IBAction func Ako(_ sender: Any) {
+    }
+    @IBAction func Himeji(_ sender: Any) {
+    }
+    @IBAction func Matue(_ sender: Any) {
+    }
+    @IBAction func Kino(_ sender: Any) {
+    }
+    @IBAction func Marugame(_ sender: Any) {
+    }
+    @IBAction func Kochi(_ sender: Any) {
+    }
+    @IBAction func Kumamoto(_ sender: Any) {
+    }
+    
+    var num = 0
+    let images = [#imageLiteral(resourceName: "Goryokaku"), #imageLiteral(resourceName: "Hirosaki"), #imageLiteral(resourceName: "Sendai"), #imageLiteral(resourceName: "Yamagata"), #imageLiteral(resourceName: "Aizuwakamatu"), #imageLiteral(resourceName: "Oshi"), #imageLiteral(resourceName: "Odawara"), #imageLiteral(resourceName: "Ueda"), #imageLiteral(resourceName: "Matumoto"), #imageLiteral(resourceName: "Kanazawa"), #imageLiteral(resourceName: "Maruoka"), #imageLiteral(resourceName: "Inuyama"), #imageLiteral(resourceName: "Nagoya"), #imageLiteral(resourceName: "Nagashino"), #imageLiteral(resourceName: "Shri"), #imageLiteral(resourceName: "Hikone"), #imageLiteral(resourceName: "Azuchi"), #imageLiteral(resourceName: "Osaka"), #imageLiteral(resourceName: "Takeda"), #imageLiteral(resourceName: "Ako"), #imageLiteral(resourceName: "Himeji"), #imageLiteral(resourceName: "Matue"), #imageLiteral(resourceName: "Kino"), #imageLiteral(resourceName: "Marukame"), #imageLiteral(resourceName: "Kochi"),#imageLiteral(resourceName: "Kumamoto")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
